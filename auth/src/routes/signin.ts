c5b17592a5e061f2f64435dbab026163ac956981
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2020-12-28 20:47:46 +0530

    Implemented SignIn flow

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,8 +1,47 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { BadRequestError } from '../errors/bad-request-error';
+import jwt from 'jsonwebtoken';
+
+import { validateRequest } from '../middlewares/validate-request';
+import { User } from '../models/user';
+import { Password } from '../services/password';
+
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send({})
-});
+router.post(
+  '/api/users/signin',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').trim().notEmpty().withMessage('Password must be valid'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      throw new BadRequestError('Invalid Credentials');
+    }
+    const passwordMatch = await Password.compare(
+      existingUser!.password,
+      password
+    );
+    if (!passwordMatch) {
+      throw new BadRequestError('Invalid Credentials');
+    }
+    // generate json web token and store on session object
+    const userJWT = jwt.sign(
+      {
+        id: existingUser!.id,
+        email: existingUser!.email,
+      },
+      process.env.JWT_KEY! // ! says we have already checked rthis key and we know this exist
+    );
+    req.session = {
+      jwt: userJWT,
+    };
+    res.status(200).send(existingUser);
+  }
+);
 
-export {router as signInRouter}
+export { router as signInRouter };
