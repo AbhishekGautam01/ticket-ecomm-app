675bc0cdc96a06267ec679e161bf5efb0b77a7b6
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2020-12-28 12:50:24 +0530

    Auth: Added a Dummy EndPoint

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,11 @@ import { json } from 'body-parser';
 
 const app = express();
 app.use(json());
+
+app.get('/api/users/currentuser', (req, res) => {
+  res.send({})
+})
+
 app.listen(3000, () => {
-  console.log('Auth-Service: Listening on Port: 3000');
+  console.log('[Auth-Service]: Listening on Port: 3000');
 });
