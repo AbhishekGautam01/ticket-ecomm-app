5a1f679c672cda1ff1eb6c0e6c6f9ead335fe4a0
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2020-12-28 22:03:09 +0530

    Refactoring for testing architecture

---
+++ auth/src/app.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './routes/current-user';
+import { signInRouter } from './routes/signin';
+import { signUpRouter } from './routes/signup';
+import { singOutRouter } from './routes/signout';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+
+const app = express();
+app.set('trust proxy', true); // to make sure express is aware it is behind proxy ingress
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false, //to disable encyption
+    secure: true, //for https
+  })
+);
+
+app.use(currentUserRouter);
+app.use(singOutRouter);
+app.use(signUpRouter);
+app.use(signInRouter);
+
+app.all('*', async (req, res, next) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+export { app };
