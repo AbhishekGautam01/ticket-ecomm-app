f26990c3c90f332d0d46a856637f4280770a31b5
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2020-12-28 14:56:44 +0530

    SignUp Validation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,29 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
-  res.send({})
-});
+router.post(
+  '/api/users/signup',
+  [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage('Password must be valid'),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
 
-export {router as signUpRouter}
+    const { email, password } = req.body;
+
+    console.log('Creating a user');
+
+    res.send({});
+  }
+);
+
+export { router as signUpRouter };
